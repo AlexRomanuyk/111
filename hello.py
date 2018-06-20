ad5dadf1a1082927aef54aea5837f1c7c7fd5429
Romanyuk Oleksii <alexromanuk2@gmail.com>
2018-06-20 09:44:27 +0000

    Initial

---
+++ hello.py
@@ -0,0 +1,14 @@
+print ("What is your name?")
+user_name = input("user name: ")
+print ("Hello, " + user_name)
+
+print ("How old are you?")
+user_age = input("user age: ")
+print ("Your age is " + user_age)
+
+user_age_nex_year = int(user_age) + 1
+print ("You will be next year " + str(user_age_nex_year))
+
+print ("Where do you live?")
+user_palace = input("user palace: ")
+print ("You live in " + user_palace)
