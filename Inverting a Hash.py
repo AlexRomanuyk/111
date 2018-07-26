086909186c79dca9630e7599c69d118d984e88c1
AlexRomanuyk <40401729+AlexRomanuyk@users.noreply.github.com>
2018-07-26 12:58:58 +0300

    Update Inverting a Hash.py

--- Inverting a Hash.py
+++ Inverting a Hash.py
@@ -1,6 +1,3 @@
 def invert_hash(dictionary):
-    inverted_dict = {}
-    for a, b in dictionary.items():
-        inverted_dict.update({b : a})
-    return inverted_dict
+     return {value: key for key, value in dictionary.items()}
     
